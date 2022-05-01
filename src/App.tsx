6de671e0efbd784c384f9b153e410feb09a30561
eric <ericposas@outlook.com>
2022-05-01 18:02:54 -0500

    simplified contract methods, cleaned up UI a bit

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,54 @@ declare var window: any;
 
 const Div = styled.div<{
   mt?: string,
-  mb?: string
+  mb?: string,
+  bg?: string,
+  radius?: string,
+  w?: string,
+  p?: string,
 }>`
+margin: auto;
+background-color: ${({ bg }) => bg ?? 'unset'};
+border-radius: ${({ radius }) => radius ?? '0px'};
+width: ${({ w }) => w ?? '500px'};
+padding: ${({ p }) => p ?? '0'};
+position: relative;
 margin-top: ${({ mt }) => mt ?? '12px'};
 margin-bottom: ${({ mb }) => mb ?? '12px'};
 `;
 
 const Button = styled.button<{
   ml?: string,
+  mr?: string,
   pt?: string,
   pb?: string,
   pl?: string,
   pr?: string,
+  display?: string,
 }>`
 padding-top: ${({ pt }) => pt ?? '10px'};
 padding-bottom: ${({ pb }) => pb ?? '10px'};
 padding-left: ${({ pl }) => pl ?? '16px'};
 padding-right: ${({ pr }) => pr ?? '16px'};
+display: ${({ display }) => display ?? 'auto'};
 background-color: cornflowerblue;
 border-radius: 4px;
 font-weight: bold;
-color: white;
+color: ${({ color }) => color ?? 'white'};;
 border: none;
 cursor: pointer;
-margin-left: ${({ ml }) => ml}
+margin-left: ${({ ml }) => ml};
+margin-right: ${({ mr }) => mr};
 `;
 
-const Input = styled.input<{}>`
-width: 50px;
+const Input = styled.input<{
+  ml?: string,
+  mr?: string,
+}>`
+width: 40px;
+font-size: 1rem;
+margin-left: ${({ ml }) => ml ?? '0'};
+margin-right: ${({ mr }) => mr ?? '0'};
 margin-right: 5px;
 `;
 
@@ -50,13 +70,13 @@ export default function App() {
     if (window.ethereum && providerRef.current === undefined) {
       providerRef.current = new ethers.providers.Web3Provider(window.ethereum)
       signerRef.current = providerRef.current.getSigner()
-      // set up MyContract
+      // set up contract, connect to methods in the ABI interface
       contractRef.current = new ethers.Contract(ContractABI.networks[4].address, ContractABI.abi, signerRef.current);
     }
   }, []);
 
   const getUsersCoinBalance = useCallback(async () => {
-    const result = await contractRef.current.getUserBal();    
+    const result = await contractRef.current.getUserBal();
     setCoinBalance(toNormalNumber(result))
   }, [contractRef]);
 
@@ -68,26 +88,31 @@ export default function App() {
   const BASE_ETHER_25 = 10000000000000000000000000;
 
   const [tokenSpendValue, setTokenSpendValue] = useState<string>('0');
-  const [transactionApproved, setTransactionApproved] = useState<boolean>(false);
   const [coinBalance, setCoinBalance] = useState<string>('0.0');
 
   const toNormalNumber = (bn: BigNumber) => ethers.utils.formatEther(bn.toString());
 
   return (
-    <div className="App">
-      <Div>
-        This app needs your permission to use your account(s)
-      </Div>
-      <Button
-        onClick={async () => {
-          if (providerRef.current) {
-            const _accounts = await providerRef.current.send('eth_requestAccounts', [])
-            setAccounts(_accounts)
-          }
-        }}
-      >
-        Request Permission
-      </Button>
+    <div className="App" >
+      {
+        accounts.length < 1 ?
+          <>
+            <Div>
+              Connect Web3 wallet
+            </Div>
+            <Button
+              onClick={async () => {
+                if (providerRef.current) {
+                  const _accounts = await providerRef.current.send('eth_requestAccounts', [])
+                  setAccounts(_accounts)
+                }
+              }}
+            >
+              Connect
+            </Button>
+          </>
+          : null
+      }
       {
         contractRef.current &&
         accounts.length > 0 && (
@@ -97,136 +122,105 @@ export default function App() {
             </Div>
             <Div
               style={{
-                backgroundColor: '#333',
+                backgroundColor: '#111',
                 color: '#fff',
                 padding: '8px',
                 width: '180px',
                 textAlign: 'center',
                 margin: '0 auto',
-                marginBottom: '20px',
-                borderRadius: '3px'
+                borderRadius: '3px',
+                marginBottom: '4px',
+                fontSize: '0.85rem'
               }}
             >
               Coin Balance: {coinBalance}
             </Div>
-            {
-              accounts.map((acct, i) => (
-                <Div key={i}>
-                  <Div
-                    style={{
-                      backgroundColor: '#333',
-                      color: '#fff',
-                      padding: '8px',
-                      width: '400px',
-                      textAlign: 'center',
-                      margin: '0 auto',
-                      marginBottom: '20px',
-                      borderRadius: '3px'
-                    }}
-                  >
-                    {acct}
-                  </Div>
-                  <div>
-                    Available "{ContractABI.contractName}" Contract Methods
-                  </div>
-                  {
-                    <Div>
-                      <Div>
-                        <Button ml='12px' pt='6px' pb='6px' pl='10px' pr='10px'
-                          onClick={async () => {
-                            // get $20 of ether value (fig. out from coingecko api perhaps), just random amt for now
-                            const result = await contractRef.current.buyCoins({ value: ethers.utils.parseEther('0.005') })
-                            console.log(
-                              result
-                            );
-                            if (providerRef && providerRef.current) {
-                              providerRef.current.once(result.hash, (transaction) => {
-                                // Emitted when the transaction has been mined
-                                console.log(
-                                  result.hash,
-                                  transaction
-                                );
-                                getUsersCoinBalance();
-                              });
-                            }
-                          }}
-                        >
-                          Buy tokens
-                        </Button>
-                        <Button ml='12px' pt='6px' pb='6px' pl='10px' pr='10px'
-                          onClick={getUsersCoinBalance}
-                        >
-                          Check Token Balance
-                        </Button>
-                        <br />
-                        <Div>
-                          <Div mt='0'>
-                            Buy some in-game items or something..
-                          </Div>
-                          <>
-                            {
-                              !transactionApproved
-                                // transactionApproved
-                                ? <Button ml='12px' pt='6px' pb='6px' pl='10px' pr='10px'
-                                  onClick={async () => {
-                                    const result = await contractRef.current.approveSpend();
-                                    console.log(
-                                      result
-                                    );
-                                    if (providerRef && providerRef.current) {
-                                      providerRef.current.once(result.hash, (transaction) => {
-                                        // Emitted when the transaction has been mined
-                                        console.log(
-                                          result.hash,
-                                          transaction
-                                        );
-                                        setTransactionApproved(true); // will need to get transaction approval upon app load to check against and set the proper state
-                                      });
-                                    }
-                                  }}
-                                >
-                                  Approval to Spend DETH
-                                </Button>
-                                : <>
-                                  <Input
-                                    min={0}
-                                    max={100}
-                                    type={'number'}
-                                    value={tokenSpendValue}
-                                    onChange={e => setTokenSpendValue(e.currentTarget.value)}
-                                  />
-                                  <Button ml='12px' pt='6px' pb='6px' pl='10px' pr='10px'
-                                    onClick={async () => {
-                                      const result = await contractRef.current.spendCoins(
-                                        (tokenSpendValue as unknown as number * BASE_COIN_18_DECIMALS).toString()
-                                      );
-                                      console.log(result);
-                                      if (providerRef && providerRef.current) {
-                                        providerRef.current.once(result.hash, (transaction) => {
-                                          // Emitted when the transaction has been mined
-                                          console.log(
-                                            result.hash,
-                                            transaction
-                                          )
-                                          setTransactionApproved(false);
-                                          getUsersCoinBalance();
-                                          setTokenSpendValue('0');
-                                        });
-                                      }
-                                    }}
-                                  >
-                                    Spend tokens
-                                  </Button>
-                                </>
-                            }
-                          </>
-                        </Div>
-                      </Div>
-                    </Div>
-                  }
-                </Div>
-              ))
-            }
+
+            <Div
+              style={{
+                backgroundColor: '#111',
+                color: '#fff',
+                padding: '8px',
+                width: '400px',
+                textAlign: 'center',
+                margin: '0 auto',
+                borderRadius: '3px',
+                fontSize: '0.85rem'
+              }}
+            >
+              {accounts[0]}
+            </Div>
+
+            <Div p='30px' mt='30px' mb='30px' radius='4px' bg='rgba(0, 0, 0, 0.1)'>
+              <Div>
+                Available "{ContractABI.contractName}" Contract Methods
+              </Div>
+              <Div>
+                Buy Some Coins
+                <Button ml='12px' pt='6px' pb='6px' pl='10px' pr='10px'
+                  onClick={async () => {
+                    // get $20 of ether value (fig. out from coingecko api perhaps), just random amt for now
+                    const result = await contractRef.current.buyCoins({ value: ethers.utils.parseEther('0.005') })
+                    console.log(
+                      result
+                    );
+                    if (providerRef && providerRef.current) {
+                      providerRef.current.once(result.hash, (transaction) => {
+                        // Emitted when the transaction has been mined
+                        console.log(
+                          result.hash,
+                          transaction
+                        );
+                        getUsersCoinBalance();
+                      });
+                    }
+                  }}
+                >
+                  Buy Coins
+                </Button>
+                <Button display='inline-block' ml='12px' pt='6px' pb='6px' pl='10px' pr='10px'
+                  onClick={async () => {
+                    await getUsersCoinBalance();
+                    window.alert(`You have ${coinBalance} COINs`)
+                  }}
+                >
+                  Check Coin Balance
+                </Button>
+              </Div>
+              <Div>
+                Spend Your Coins
+                <Input
+                  ml='10px'
+                  min={0}
+                  max={100}
+                  type={'number'}
+                  value={tokenSpendValue}
+                  onChange={e => setTokenSpendValue(e.currentTarget.value)}
+                />
+                <Button ml='2px' pt='6px' pb='6px' pl='10px' pr='10px'
+                  onClick={async () => {
+                    const result = await contractRef.current.spendCoins(
+                      (tokenSpendValue as unknown as number * BASE_COIN_18_DECIMALS).toString()
+                    );
+                    console.log(result);
+                    if (providerRef && providerRef.current) {
+                      providerRef.current.once(result.hash, (transaction) => {
+                        // Emitted when the transaction has been mined
+                        console.log(
+                          result.hash,
+                          transaction
+                        );
+                        getUsersCoinBalance();
+                        setTokenSpendValue('0');
+                      });
+                    }
+                  }}
+                >
+                  Spend Coins
+                </Button>
+              </Div>
+            </Div>
+
           </>
         )
       }
